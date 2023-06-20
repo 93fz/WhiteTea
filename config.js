605c1ae7b57a1c297bd5ce2acae5fa3f84549e88
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-06-20 09:02:43 -0400

    add files

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    hp: '5', // between 1 and 21, if you're starting
+    time: '42', // between 3 and 50, if you're starting
+    channelId: '', // channel ID to play in
+    token: '', // token to use
+    selfStart: true, // if you should start the game (singleplayer), set to false to play publicly
+    alreadySent: false, // set to true if the 'black teaword will start' embed has already been sent
+    mudaePrefix: '$',
+    delays: { // delays
+        msgSend: {
+            min: 500, // minimum time in ms
+            max: 2000 // max time in ms
+        }
+    }
+}
