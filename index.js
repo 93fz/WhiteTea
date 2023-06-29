c7aaa3eb1bcbd991fe58d25de429631fa12156d7
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-06-29 14:33:04 -0400

    fix reaction collector & update visuals

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+console.log(`☕ | WhiteTea has started!`);
+console.log(`☕ | WARNING: recent errors cause WhiteTea to be unstable in long games.`);
+console.log(`☕ | Join the Support Server: discord.gg/5dR3ca38tB\n`);
+
 let bot = require('discord.js-selfbot-v13');
 let config = require('./config.js');
 let words = require('./words.js');
@@ -7,10 +11,17 @@ let rndInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 let client = new bot.Client({
     checkUpdate: false
-})
+});
+
+if ((config.selfStart == config.alreadySent) && config.selfStart) {
+    console.log(`☕ | The config appears to have an issue.`);
+    console.log(`☕ | Please join the Discord server for support.`);
+    
+    process.exit(0);
+}
 
 client.on('ready', async () => {
-    console.log(`☕ | Connected to @${client.user.username}!`);
+    console.log(`☕ | Connected to @${client.user.username}!\n`);
     
     let channel = client.channels.cache.get(config.channelId);
     let mudaePrefix = config.mudaePrefix;
@@ -18,6 +29,11 @@ client.on('ready', async () => {
     client.on('messageCreate', async (msg) => {
         if (msg.author.id !== '432610292342587392') return;
         if (msg.channelId !== config.channelId) return;
+
+        if (config.selfStart && msg.content?.includes('Another command is in activity, type $exitgame to stop it.')) {
+            console.log(`☕ | There is already an ongoing game. Quitting...`);
+            process.exit(0);
+        };
 
         if (msg.embeds?.[0]?.title === 'The Black Teaword will start!') {
             msg.react('✅');
@@ -30,19 +46,23 @@ client.on('ready', async () => {
 
             (async function sendWord() {
                 let pickFrom = words.filter(a => a.search(msg.content.split('**')[1].toLowerCase()) > -1);
-		let word = pickFrom[rndInt(0, (pickFrom.length-1))];
+				let word = pickFrom[rndInt(0, (pickFrom.length-1))];
                 let sentMsg = await msg.channel.send(word)
 
-                let filter = (reaction, user) => reaction.emoji.name === '✅' && user.id !== '432610292342587392';
-                let collector = sentMsg.createReactionCollector(filter, { time: 3000 });
+                let collector = sentMsg.createReactionCollector({
+		    		filter: (reaction, user) => reaction.emoji.name === '✅' && user.id === '432610292342587392',
+		    		time: 5000
+				});
 
                 collector.on('collect', (reaction, user) => {
                   words.splice(words.indexOf(sentMsg.content), 1);
-                  console.log(`☕ | Prompt: ${msg.content.split('**')[1].toLowerCase()} | Sent: ${word}`)
+                  console.log(`☕ | Prompt: ${msg.content.split('**')[1].toLowerCase()} | Sent: ${word}`);
                 });
                     
                 collector.on('end', async (collected) => {
-                    if (collected.size < 1) await sendWord();
+                    if (collected.size > 0) return;
+                    console.log(`☕ | Prompt: ${msg.content.split('**')[1].toLowerCase()} | word was not in db, skipping to new...`);
+		    		await sendWord();
                 });
             })();
         };
@@ -54,12 +74,13 @@ client.on('ready', async () => {
         channel.send(`${mudaePrefix}hp ${config.hp}`);
         await sleep(rndInt(config.delays.msgSend.min, config.delays.msgSend.max));
         channel.send(`${mudaePrefix}time ${config.time}`);
+        console.log(`☕ | Sent the Self-Start mode messages.`);
     };
 
-    if (!config.alreadySent) {
+    if (config.alreadySent) {
         channel.messages.fetch({
-	    limit: 100
-	}).then((messages) => {
+			limit: 100
+		}).then((messages) => {
             let byMudae = messages.filter(a => a.author.id === '432610292342587392');
             let isBlackteaStart = byMudae.filter(a => a.embeds[0]?.title).filter(a => a.embeds[0].title === 'The Black Teaword will start!');
             isBlackteaStart.first().react('✅');
